0667dc490e5f4b1f18064cfeae6d915c8ac5fe8c
Cassy Culkins and Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-25 09:29:19 -0800

    write spec for Place constructor

---
+++ spec/specs.js
@@ -0,0 +1,9 @@
+describe('Place', function() {
+  it("creates a new place with the given properties", function() {
+    var testPlace = new Place("Oregon", ["Multnomah Falls", "Crater Lake"]);
+    expect(testPlace.location).to.equal("Oregon");
+    expect(testPlace.landmarks).to.eql(["Multnomah Falls", "Crater Lake"]);
+    expect(testPlace.timeOfYear).to.equal();
+    expect(testPlace.notes).to.equal();
+  });
+});
