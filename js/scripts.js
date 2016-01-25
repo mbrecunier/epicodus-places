bdc3d0dbc67c6027ad79eb6da69b0c1bb5d354a0
Cassy Culkins and Michelle Brecunier <cassandra.culkins@gmail.com>
2016-01-25 10:25:36 -0800

    hide show-placeInfo div

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,16 @@ function Place(region, landmarks, timeOfYear, notes) {
   this.timeOfYear = timeOfYear;
   this.notes = notes;
 }
+$(document).ready(function() {
+  $("form#new-place").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedRegion = $("input#new-region").val();
+    var inputtedLandmarks = [$("input#new-landmark1").val(), $("input#new-landmark2").val(), $("input#new-landmark3").val()];
+    var inputtedTimeOfYear = $("input#new-timeOfYear").val();
+    var inputtedNotes = $("input#new-notes").val();
+    var newPlace = new Place(inputtedRegion, inputtedLandmarks, inputtedTimeOfYear, inputtedNotes);
+
+
+  }
+}
