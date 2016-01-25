071d9843424d92540da11b90176336280b8a3ebb
Cassy Culkins and Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-25 10:26:43 -0800

    display place info on click

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,21 @@ $(document).ready(function() {
     var inputtedRegion = $("input#new-region").val();
     var inputtedLandmarks = [$("input#new-landmark1").val(), $("input#new-landmark2").val(), $("input#new-landmark3").val()];
     var inputtedTimeOfYear = $("input#new-timeOfYear").val();
-    var inputtedNotes = $("input#new-notes").val();
+    var inputtedNotes = $("textarea#new-notes").val();
     var newPlace = new Place(inputtedRegion, inputtedLandmarks, inputtedTimeOfYear, inputtedNotes);
 
+    $("ul#placeList").append("<li><span class='place'>" + newPlace.region + "</span></li>");
 
-  }
-}
+    $(".place").last().click(function() {
+      $("#show-placeInfo").show();
+      $("#show-placeInfo h2").text(newPlace.region);
+      $(".region").text(newPlace.region);
+      $(".landmark1").text(newPlace.landmarks[0]);
+      $(".landmark2").text(newPlace.landmarks[1]);
+      $(".landmark3").text(newPlace.landmarks[2]);
+      $(".timeOfYear").text(newPlace.timeOfYear);
+      $(".notes").text(newPlace.notes);
+      })
+
+  });
+});
